988ca0a835f4e58c543bbf82c0bc9690f6686c2d
ivica <blizujemars@gmail.com>
2023-12-26 16:58:10 +0100

    add writeJSON test

--- cmd/api/helpers_test.go
+++ cmd/api/helpers_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/go-chi/chi/v5"
@@ -25,6 +26,28 @@ func Test_app_readIDParam(t *testing.T) {
 		chiCtx.URLParams.Add("id", test.paramId)
 		req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
 		_, err := app.readIDParam(req)
+		if !test.expectError && err != nil {
+			t.Errorf("%s: expected no err, but got one: %q", test.name, err)
+		}
+		if test.expectError && err == nil {
+			t.Errorf("%s: expected err, but didn't get one", test.name)
+		}
+	}
+}
+
+func Test_app_writeJSON(t *testing.T) {
+	tests := []struct {
+		name        string
+		data        any
+		expectError bool
+	}{
+		{"valid data", map[string]string{"test": "yes"}, false},
+		{"invalid data", func() {}, true},
+	}
+
+	for _, test := range tests {
+		rr := httptest.NewRecorder()
+		err := app.writeJSON(rr, http.StatusOK, test.data, nil)
 		if !test.expectError && err != nil {
 			t.Errorf("%s: expected no err, but got one: %q", test.name, err)
 		}
