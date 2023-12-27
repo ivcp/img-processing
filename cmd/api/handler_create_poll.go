bb41e2ff1a2377fece1bcaa2334ac5c666717768
ivica <blizujemars@gmail.com>
2023-12-27 21:13:58 +0100

    add input struct, readJSON

--- cmd/api/handler_create_poll.go
+++ cmd/api/handler_create_poll.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func (app *application) createPollHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "create a new poll")
+	var input struct {
+		Question    string    `json:"question"`
+		Description string    `json:"desription"`
+		Options     []string  `json:"options"`
+		ExpiresAt   time.Time `json:"expires_at"`
+	}
+
+	err := app.readJSON(r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	fmt.Fprintf(w, "%+v\n", input)
 }
