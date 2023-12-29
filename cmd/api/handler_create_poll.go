f1a85d3a8c03724c4e05bc39fcea0de7eaf2f3f0
ivica <blizujemars@gmail.com>
2023-12-29 18:43:14 +0100

    add validation for input

--- cmd/api/handler_create_poll.go
+++ cmd/api/handler_create_poll.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/ivcp/polls/internal/data"
+	"github.com/ivcp/polls/internal/validator"
 )
 
 func (app *application) createPollHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
 		Question    string    `json:"question"`
-		Description string    `json:"desription"`
+		Description string    `json:"description"`
 		Options     []string  `json:"options"`
 		ExpiresAt   time.Time `json:"expires_at"`
 	}
@@ -17,6 +20,24 @@ func (app *application) createPollHandler(w http.ResponseWriter, r *http.Request
 	err := app.readJSON(w, r, &input)
 	if err != nil {
 		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	options := []data.PollOption{}
+	for _, option := range input.Options {
+		options = append(options, data.PollOption{Value: option})
+	}
+
+	poll := &data.Poll{
+		Question:    input.Question,
+		Description: input.Description,
+		Options:     options,
+		ExpiresAt:   input.ExpiresAt,
+	}
+
+	v := validator.New()
+	if data.ValidatePoll(v, poll); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
 
