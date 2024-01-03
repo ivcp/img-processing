4862392a603c3490a06b1909cf68fe2f3f0ca9bc
ivica <blizujemars@gmail.com>
2024-01-03 11:56:43 +0100

    connect to db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,6 +7,9 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 const version = "1.0.0"
@@ -14,6 +17,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db   struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -25,6 +31,11 @@ func main() {
 	var cfg config
 	flag.IntVar(&cfg.port, "port", 8080, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.StringVar(
+		&cfg.db.dsn,
+		"db-dsn",
+		os.Getenv("POLLS_DB_DSN"),
+		"DSN string")
 	flag.Parse()
 
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
@@ -33,6 +44,12 @@ func main() {
 		config: cfg,
 		logger: logger,
 	}
+
+	db, err := app.connectToDB()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer db.Close()
 
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", cfg.port),
@@ -43,6 +60,6 @@ func main() {
 	}
 
 	logger.Printf("Starting %s server on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
 }
