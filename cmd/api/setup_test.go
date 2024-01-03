fe031e8c3d98d95d5ef5a0feaf06834a40df4190
ivica <blizujemars@gmail.com>
2024-01-03 18:14:15 +0100

    add test db connection

--- cmd/api/setup_test.go
+++ cmd/api/setup_test.go
@@ -3,10 +3,13 @@ package main
 import (
 	"os"
 	"testing"
+
+	"github.com/ivcp/polls/internal/data"
 )
 
 var app application
 
 func TestMain(m *testing.M) {
+	app.models = data.NewMockModels()
 	os.Exit(m.Run())
 }
