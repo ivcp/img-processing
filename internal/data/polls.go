eb738cc5c072b7331ab1e3b777dfa5a3cc83b8ab
ivica <blizujemars@gmail.com>
2024-01-03 16:59:03 +0100

    add Poll model and methods

--- internal/data/polls.go
+++ internal/data/polls.go
@@ -1,9 +1,11 @@
 package data
 
 import (
+	"context"
 	"time"
 
 	"github.com/ivcp/polls/internal/validator"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Poll struct {
@@ -34,4 +36,73 @@ func ValidatePoll(v *validator.Validator, poll *Poll) {
 	v.Check(validator.Unique(poll.Options), "options", "must not contain duplicate values")
 	v.Check(!poll.ExpiresAt.IsZero(), "expires_at", "must be provided")
 	v.Check(poll.ExpiresAt.After(time.Now().Add(time.Minute)), "expires_at", "must be more than a minute in the future")
+}
+
+type PollModel struct {
+	DB *pgxpool.Pool
+}
+
+func (p PollModel) Insert(poll *Poll) error {
+	queryPoll := `
+		INSERT INTO polls (question, description, expires_at)
+		VALUES ($1, $2, $3)
+		RETURNING id, created_at, updated_at, version;				
+		`
+
+	queryOption := `
+		INSERT INTO poll_options (value, poll_id, position, vote_count)
+		VALUES ($1, $2, $3, $4)		
+		RETURNING id;
+	`
+
+	args := []any{poll.Question, poll.Description, poll.ExpiresAt}
+
+	err := p.DB.QueryRow(
+		context.Background(), queryPoll, args...,
+	).Scan(&poll.ID, &poll.CreatedAt, &poll.UpdatedAt, &poll.Version)
+
+	for i, option := range poll.Options {
+		args := []any{option.Value, poll.ID, option.Position, option.VoteCount}
+		err := p.DB.QueryRow(
+			context.Background(), queryOption, args...,
+		).Scan(&poll.Options[i].ID)
+		if err != nil {
+			return err
+		}
+	}
+
+	return err
+}
+
+func (p PollModel) Get(id int) (*Poll, error) {
+	return nil, nil
+}
+
+func (p PollModel) Update(poll *Poll) error {
+	return nil
+}
+
+func (p PollModel) Delete(id int) error {
+	return nil
+}
+
+// mocks
+type MockPollModel struct {
+	DB *pgxpool.Pool
+}
+
+func (p MockPollModel) Insert(poll *Poll) error {
+	return nil
+}
+
+func (p MockPollModel) Get(id int) (*Poll, error) {
+	return nil, nil
+}
+
+func (p MockPollModel) Update(poll *Poll) error {
+	return nil
+}
+
+func (p MockPollModel) Delete(id int) error {
+	return nil
 }
