433136d7c6b9e66a3a8e557eb7bcc0dc452d4c70
ivica <blizujemars@gmail.com>
2024-01-06 14:15:41 +0100

    add test for min num of options

--- cmd/api/handler_create_poll_test.go
+++ cmd/api/handler_create_poll_test.go
@@ -22,15 +22,21 @@ func Test_app_createPollHandler(t *testing.T) {
 		expectedBody   string
 	}{
 		{
-			name:           "epmty question",
-			json:           `{"question":"", "options":[{"value":"first","position":0}]}`,
+			name: "epmty question",
+			json: `{
+				"question":"", 
+				"options":[{"value":"first","position":0},{"value":"second","position":1}]
+				}`,
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"question":"must not be empty"}}`,
 		},
 		{
 			name: "question too long",
 			json: fmt.Sprintf(
-				`{"question":%q, "options":[{"value":"first","position":0}]}`,
+				`{
+				"question":%q, 
+				"options":[{"value":"first","position":0},{"value":"second","position":1}]
+				}`,
 				questionInvalid,
 			),
 			expectedStatus: http.StatusUnprocessableEntity,
@@ -39,7 +45,10 @@ func Test_app_createPollHandler(t *testing.T) {
 		{
 			name: "description too long",
 			json: fmt.Sprintf(
-				`{"question":"Test?", "description":%q, "options":[{"value":"first","position":0}]}`,
+				`{
+					"question":"Test?", 
+					"description":%q, 
+					"options":[{"value":"first","position":0},{"value":"second","position":1}]}`,
 				descriptionInvalid,
 			),
 			expectedStatus: http.StatusUnprocessableEntity,
@@ -47,60 +56,98 @@ func Test_app_createPollHandler(t *testing.T) {
 		},
 		{
 			name: "description provided but empty string",
-			json: `{"question":"Test?", "description":" ", "options":[{"value":"first","position":0}]}`,
-
+			json: `{
+				"question":"Test?", 
+				"description":" ", 
+				"options":[{"value":"first","position":0},{"value":"second","position":1}]
+				}`,
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"description":"must not be empty"}}`,
 		},
 		{
 			name: "expires_at invalid",
 			json: fmt.Sprintf(
-				`{"question":"Test?", "options":[{"value":"first","position":0}],"expires_at":%q}`,
+				`{
+					"question":"Test?", 
+					"options":[{"value":"first","position":0},{"value":"second","position":1}],
+					"expires_at":%q
+					}`,
 				expiresInvalid,
 			),
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"expires_at":"must be more than a minute in the future"}}`,
 		},
 		{
-			name:           "duplicate options",
-			json:           `{"question":"Test?", "options":[{"value":"first","position":0}, {"value":"first","position":1}]}`,
+			name: "only one option provided",
+			json: `{
+				"question":"Test?", 
+				"options":[{"value":"first","position":0}]
+				}`,
+			expectedStatus: http.StatusUnprocessableEntity,
+			expectedBody:   `{"error":{"options":"must contain at least two options"}}`,
+		},
+		{
+			name: "duplicate options",
+			json: `{
+				"question":"Test?", 
+				"options":[{"value":"first","position":0},{"value":"first","position":1}]
+				}`,
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"options":"must not contain duplicate values"}}`,
 		},
 		{
-			name:           "duplicate option positions",
-			json:           `{"question":"Test?", "options":[{"value":"first","position":0}, {"value":"second","position":0}]}`,
+			name: "duplicate option positions",
+			json: `{
+				"question":"Test?", 
+				"options":[{"value":"first","position":0}, {"value":"second","position":0}]
+				}`,
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"options":"positions must be unique"}}`,
 		},
 		{
-			name:           "invalid option positions",
-			json:           `{"question":"Test?", "options":[{"value":"first","position":2}, {"value":"second","position":0}]}`,
+			name: "invalid option positions",
+			json: `{
+				"question":"Test?", 
+				"options":[{"value":"first","position":2}, {"value":"second","position":0}]
+				}`,
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"options":"position must not excede the number of options"}}`,
 		},
 		{
-			name:           "invalid option positions",
-			json:           `{"question":"Test?", "options":[{"value":"first","position":-1}, {"value":"second","position":0}]}`,
+			name: "invalid option positions",
+			json: `{
+				"question":"Test?", 
+				"options":[{"value":"first","position":-1}, {"value":"second","position":0}]
+				}`,
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"options":"position must be greater or equal to 0"}}`,
 		},
 		{
-			name:           "invalid empty option",
-			json:           `{"question":"Test?", "options":[{"value":" ","position":0}]}`,
+			name: "invalid empty option",
+			json: `{
+				"question":"Test?", 
+				"options":[{"value":" ","position":0}, {"value":"second","position":1}]
+				}`,
 			expectedStatus: http.StatusUnprocessableEntity,
 			expectedBody:   `{"error":{"options":"option values must not be empty"}}`,
 		},
 		{
-			name:           "invalid json field type",
-			json:           `{"question":1, "options":[{"value":"first","position":0}]}`,
+			name: "invalid json field type",
+			json: `{
+				"question":1, 
+				"options":[{"value":"first","position":0}]
+				}`,
 			expectedStatus: http.StatusBadRequest,
 			expectedBody:   `{"error":"body contains incorrect JSON type for field \"question\""}`,
 		},
 		{
 			name: "insert poll valid",
 			json: fmt.Sprintf(
-				`{"question":"Test?", "options":[{"value":"first", "position":0}],"expires_at":%q}`,
+				`{
+					"question":"Test?", 
+					"options":[{"value":"first","position":0}, {"value":"second","position":1}],
+					"expires_at":%q
+					}`,
 				expiresValid,
 			),
 			expectedStatus: http.StatusCreated,
