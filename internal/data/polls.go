eecda2fc9e8d7a087c23d54dd0f8e647905d9833
ivica <blizujemars@gmail.com>
2024-01-06 13:40:30 +0100

    add validation for empty string option and description

--- internal/data/polls.go
+++ internal/data/polls.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/ivcp/polls/internal/validator"
@@ -31,9 +32,12 @@ type PollOption struct {
 }
 
 func ValidatePoll(v *validator.Validator, poll *Poll) {
-	v.Check(poll.Question != "", "question", "must not be empty")
+	v.Check(strings.TrimSpace(poll.Question) != "", "question", "must not be empty")
 	v.Check(len(poll.Question) <= 500, "question", "must not be more than 500 bytes long")
 	v.Check(len(poll.Description) <= 1000, "description", "must not be more than 1000 bytes long")
+	if poll.Description != "" {
+		v.Check(strings.TrimSpace(poll.Description) != "", "description", "must not be empty")
+	}
 	v.Check(poll.Options != nil, "options", "must be provided")
 	v.Check(len(poll.Options) >= 1, "options", "must contain at least one option")
 	var optValues []string
@@ -44,6 +48,9 @@ func ValidatePoll(v *validator.Validator, poll *Poll) {
 	}
 	v.Check(validator.Unique(optValues), "options", "must not contain duplicate values")
 	v.Check(validator.Unique(optPositions), "options", "positions must be unique")
+	for _, o := range optValues {
+		v.Check(strings.TrimSpace(o) != "", "options", "option values must not be empty")
+	}
 	for _, p := range optPositions {
 		v.Check(p >= 0, "options", "position must be greater or equal to 0")
 		v.Check(p <= len(poll.Options)-1, "options", "position must not excede the number of options")
