421724394537d496130c6454074a699e50641546
ivica <blizujemars@gmail.com>
2024-01-06 18:22:06 +0100

    add poll option model

--- internal/data/models.go
+++ internal/data/models.go
@@ -8,25 +8,35 @@ import (
 
 var ErrRecordNotFound = errors.New("record not found")
 
-// We'll add other models to this,
-// like a UserModel
 type Models struct {
-	Polls interface {
-		Insert(poll *Poll) error
-		Get(id int) (*Poll, error)
-		Update(poll *Poll) error
-		Delete(id int) error
-	}
+	Polls       Polls
+	PollOptions PollOptions
+}
+
+type Polls interface {
+	Insert(poll *Poll) error
+	Get(id int) (*Poll, error)
+	Update(poll *Poll) error
+	Delete(id int) error
+}
+
+type PollOptions interface {
+	GetAllByPollID(pollID int) ([]*PollOption, error)
+	Insert(pollOption *PollOption, pollID int) error
+	Update(pollOption *PollOption) error
+	Delete(id int) error
 }
 
 func NewModels(db *pgxpool.Pool) Models {
 	return Models{
-		Polls: PollModel{DB: db},
+		Polls:       PollModel{DB: db},
+		PollOptions: PollOptionModel{DB: db},
 	}
 }
 
 func NewMockModels() Models {
 	return Models{
-		Polls: MockPollModel{},
+		Polls:       MockPollModel{},
+		PollOptions: MockPollOptionModel{},
 	}
 }
