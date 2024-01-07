37301bac8e0c290984edb7a2b4f1af4a5c8e7d58
ivica <blizujemars@gmail.com>
2024-01-07 18:22:29 +0100

    add bulk insert poll options to Insert, join query to Get

--- internal/data/polls.go
+++ internal/data/polls.go
@@ -20,13 +20,20 @@ type Poll struct {
 	ExpiresAt   ExpiresAt     `json:"expires_at"`
 	Version     int           `json:"version"`
 }
+type PollOption struct {
+	ID    int    `json:"id"`
+	Value string `json:"value"`
+	// Position of option in the list, starting at 0
+	Position  int `json:"position"`
+	VoteCount int `json:"vote_count"`
+}
 
 type PollModel struct {
 	DB *pgxpool.Pool
 }
 
 func (p PollModel) Insert(poll *Poll) error {
-	queryPoll := `
+	query := `
 		INSERT INTO polls (question, description, expires_at)
 		VALUES ($1, $2, $3)
 		RETURNING id, created_at, updated_at, version;				
@@ -34,9 +41,65 @@ func (p PollModel) Insert(poll *Poll) error {
 
 	args := []any{poll.Question, poll.Description, poll.ExpiresAt.Time}
 
-	return p.DB.QueryRow(
-		context.Background(), queryPoll, args...,
+	err := p.DB.QueryRow(
+		context.Background(), query, args...,
 	).Scan(&poll.ID, &poll.CreatedAt, &poll.UpdatedAt, &poll.Version)
+	if err != nil {
+		return fmt.Errorf("insert poll: %d", err)
+	}
+
+	rows := make([][]any, 0, len(poll.Options))
+
+	for _, opt := range poll.Options {
+		rows = append(rows, []any{opt.Value, poll.ID, opt.Position, opt.VoteCount})
+	}
+
+	_, err = p.DB.CopyFrom(
+		context.Background(),
+		pgx.Identifier{"poll_options"},
+		[]string{"value", "poll_id", "position", "vote_count"},
+		pgx.CopyFromRows(rows),
+	)
+
+	if err != nil {
+		return fmt.Errorf("insert poll options: %d", err)
+	}
+
+	queryOptions := `
+			SELECT id, value, position, vote_count
+			FROM poll_options
+			WHERE poll_id = $1;
+		`
+
+	options := make([]*PollOption, 0, len(poll.Options))
+
+	rowsOpts, err := p.DB.Query(context.Background(), queryOptions, poll.ID)
+	if err != nil {
+		return fmt.Errorf("insert poll - get poll options: %w", err)
+	}
+	defer rowsOpts.Close()
+
+	for rowsOpts.Next() {
+		var pollOption PollOption
+		err := rowsOpts.Scan(
+			&pollOption.ID,
+			&pollOption.Value,
+			&pollOption.Position,
+			&pollOption.VoteCount,
+		)
+		if err != nil {
+			return fmt.Errorf("insert poll - get poll options: %w", err)
+		}
+		options = append(options, &pollOption)
+	}
+	err = rowsOpts.Err()
+	if err != nil {
+		return fmt.Errorf("insert poll - get poll options: %w", err)
+	}
+
+	poll.Options = options
+
+	return nil
 }
 
 func (p PollModel) Get(id int) (*Poll, error) {
@@ -45,22 +108,15 @@ func (p PollModel) Get(id int) (*Poll, error) {
 	}
 
 	query := `
-		SELECT id, question, description, created_at, 
-		updated_at, expires_at, version		
-		FROM polls 	
-		WHERE id = $1;
+		SELECT p.id, p. question, p.description, p.created_at, 
+		p.updated_at, p.expires_at, p.version,
+		po.id, po.value, po.position, po.vote_count
+		FROM polls p
+		JOIN poll_options po ON p.id = po.poll_id
+		WHERE p.id = $1;
 	`
-	var poll Poll
 
-	err := p.DB.QueryRow(context.Background(), query, id).Scan(
-		&poll.ID,
-		&poll.Question,
-		&poll.Description,
-		&poll.CreatedAt,
-		&poll.UpdatedAt,
-		&poll.ExpiresAt.Time,
-		&poll.Version,
-	)
+	rows, err := p.DB.Query(context.Background(), query, id)
 	if err != nil {
 		switch {
 		case errors.Is(err, pgx.ErrNoRows):
@@ -69,6 +125,55 @@ func (p PollModel) Get(id int) (*Poll, error) {
 			return nil, fmt.Errorf("get poll: %w", err)
 		}
 	}
+	defer rows.Close()
+
+	var poll Poll
+	var options []*PollOption
+
+	first := true
+	for rows.Next() {
+		var option PollOption
+		if first {
+			err = rows.Scan(
+				&poll.ID,
+				&poll.Question,
+				&poll.Description,
+				&poll.CreatedAt,
+				&poll.UpdatedAt,
+				&poll.ExpiresAt.Time,
+				&poll.Version,
+				&option.ID,
+				&option.Value,
+				&option.Position,
+				&option.VoteCount,
+			)
+		} else {
+			err = rows.Scan(
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				nil,
+				&option.ID,
+				&option.Value,
+				&option.Position,
+				&option.VoteCount,
+			)
+		}
+		if err != nil {
+			return nil, fmt.Errorf("get poll - scan: %w", err)
+		}
+		options = append(options, &option)
+		first = false
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, fmt.Errorf("get poll: %w", err)
+	}
+
+	poll.Options = options
 
 	return &poll, nil
 }
