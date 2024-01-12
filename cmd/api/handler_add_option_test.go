e5614a6c9c78f3f6606650805b5333893c9c0bb9
ivica <blizujemars@gmail.com>
2024-01-12 14:31:40 +0100

    add tests for addPollOptionHandler

---
+++ cmd/api/handler_add_option_test.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/go-chi/chi/v5"
+)
+
+func Test_app_addPollOptionHandler(t *testing.T) {
+	tests := []struct {
+		name           string
+		id             string
+		json           string
+		expectedStatus int
+		expectedBody   string
+	}{
+		{
+			name:           "valid add option",
+			id:             "1",
+			json:           `{"value":"test", "position":2}`,
+			expectedStatus: http.StatusCreated,
+			expectedBody:   "option added successfully",
+		},
+		{
+			name:           "option already exists",
+			id:             "1",
+			json:           `{"value":"Two", "position":2}`,
+			expectedStatus: http.StatusUnprocessableEntity,
+			expectedBody:   "must not contain duplicate values",
+		},
+		{
+			name:           "position not unique",
+			id:             "1",
+			json:           `{"value":"test", "position":1}`,
+			expectedStatus: http.StatusUnprocessableEntity,
+			expectedBody:   "positions must be unique",
+		},
+		{
+			name:           "unexisting poll",
+			id:             "9",
+			json:           `{"value":"test", "position":2}`,
+			expectedStatus: http.StatusNotFound,
+			expectedBody:   "the requested resource could not be found",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			req, _ := http.NewRequest(http.MethodPost, "/", strings.NewReader(test.json))
+			chiCtx := chi.NewRouteContext()
+			chiCtx.URLParams.Add("id", test.id)
+			req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+			rr := httptest.NewRecorder()
+			handler := http.HandlerFunc(app.addPollOptionHandler)
+			handler.ServeHTTP(rr, req)
+			if rr.Code != test.expectedStatus {
+				t.Errorf("expected status %d, but got %d", test.expectedStatus, rr.Code)
+			}
+			if !strings.Contains(rr.Body.String(), test.expectedBody) {
+				t.Errorf("expected body to contain %q, but got %q", test.expectedBody, rr.Body)
+			}
+		})
+	}
+}
