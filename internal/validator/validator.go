05e817aa326f4ae36ff30631d1b8b30d9374b843
ivica <blizujemars@gmail.com>
2024-01-17 10:54:50 +0100

    add PermittedValue

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -33,3 +33,12 @@ func Unique[T comparable](values []T) bool {
 	}
 	return len(values) == len(uniqueValues)
 }
+
+func PermittedValue[T comparable](value T, permittedValues ...T) bool {
+	for i := range permittedValues {
+		if value == permittedValues[i] {
+			return true
+		}
+	}
+	return false
+}
