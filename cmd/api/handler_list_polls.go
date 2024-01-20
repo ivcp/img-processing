40fefebb3ae37b0f3bba39f5879d59628c0ec7b9
ivica <blizujemars@gmail.com>
2024-01-20 11:28:02 +0100

    add list polls handler

---
+++ cmd/api/handler_list_polls.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ivcp/polls/internal/data"
+	"github.com/ivcp/polls/internal/validator"
+)
+
+func (app *application) listPollsHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Search string
+		data.Filters
+	}
+
+	v := validator.New()
+
+	qs := r.URL.Query()
+
+	input.Search = app.readString(qs, "search", "")
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
+	input.Filters.Sort = app.readString(qs, "sort", "-created_at")
+	input.Filters.SortSafelist = []string{"created_at", "question", "-created_at", "-question"}
+
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	polls, err := app.models.Polls.GetAll(input.Search, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	if err := app.writeJSON(w, http.StatusOK, envelope{"polls": polls}, nil); err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
