ae9b78ac3ef78ea85d899383b1e6decab4d9d7b4
ivica <blizujemars@gmail.com>
2024-02-04 15:51:05 +0100

    fix drop table error

--- migrations/00004_ips.sql
+++ migrations/00004_ips.sql
@@ -2,12 +2,12 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS ips (   
     id bigserial PRIMARY KEY,
-    ip inet UNIQUE NOT NULL,
+    ip inet NOT NULL,
     poll_id bigserial REFERENCES polls (id) ON DELETE CASCADE
 );
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-DROP TABLE IF EXISTS poll_options;
+DROP TABLE IF EXISTS ips;
 -- +goose StatementEnd
