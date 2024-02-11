659a85f9f97b2cecbd6f368fca7d477000400059
ivica <blizujemars@gmail.com>
2024-02-11 20:16:28 +0100

    rename

---
+++ internal/data/validate_results_visibility.go
@@ -0,0 +1,12 @@
+package data
+
+import "github.com/ivcp/polls/internal/validator"
+
+type ResultsVisibility struct {
+	Value         string
+	ValueSafelist []string
+}
+
+func ValidateResultsVisibility(v *validator.Validator, f ResultsVisibility) {
+	v.Check(validator.PermittedValue(f.Value, f.ValueSafelist...), "results_visibility", "invalid results_visibility value")
+}
