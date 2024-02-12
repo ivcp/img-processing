a07daa3b33ad5364027dc49a38e1033f9f9c8ecb
ivica <blizujemars@gmail.com>
2024-02-12 10:56:44 +0100

    add tests for after_vote and after_deadline cases

--- cmd/api/handler_show_results_test.go
+++ cmd/api/handler_show_results_test.go
@@ -13,10 +13,37 @@ func Test_app_showResultsHandler(t *testing.T) {
 	tests := []struct {
 		name           string
 		pollID         string
+		ip             string
 		expectedStatus int
 	}{
-		{"show results valid", "1", http.StatusOK},
-		{"invalid poll id", "99", http.StatusNotFound},
+		{
+			name:           "show results valid",
+			pollID:         "1",
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:           "invalid poll id",
+			pollID:         "99",
+			expectedStatus: http.StatusNotFound,
+		},
+		{
+			name:           "don't show results bofore voting",
+			pollID:         "35",
+			ip:             "10.10.10.10:10",
+			expectedStatus: http.StatusForbidden,
+		},
+		{
+			name:           "show results after voting",
+			pollID:         "35",
+			ip:             "0.0.0.1:0",
+			expectedStatus: http.StatusOK,
+		},
+		{
+			name:           "don't show results before deadline",
+			pollID:         "36",
+			ip:             "0.0.0.1:0",
+			expectedStatus: http.StatusForbidden,
+		},
 	}
 
 	for _, test := range tests {
@@ -25,6 +52,7 @@ func Test_app_showResultsHandler(t *testing.T) {
 			chiCtx := chi.NewRouteContext()
 			chiCtx.URLParams.Add("pollID", test.pollID)
 			req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, chiCtx))
+			req.RemoteAddr = test.ip
 			rr := httptest.NewRecorder()
 			handler := http.HandlerFunc(app.showResultsHandler)
 			handler.ServeHTTP(rr, req)
